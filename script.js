6d3540af5d5415c5ebeb67b5e785dbf528c517c1
nicostar-lab <nicostardeteh00@gmail.com>
2025-11-15 01:53:40 +0000

    new

---
+++ script.js
@@ -0,0 +1,25 @@
+const gravityFactors = {
+            Mercure: 0.38,
+            Vénus: 0.91,
+            Mars: 0.38,
+            Jupiter: 2.34,
+            Saturne: 1.06,
+            Uranus: 0.92,
+            Neptune: 1.19
+        };
+
+        document.getElementById('weightForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const earthWeight = parseFloat(document.getElementById('earthWeight').value);
+            const planet = document.getElementById('planet').value;
+            const image = document.querySelector('img');
+            image.src = `src/asset/${planet.toLowerCase()}.png`;
+            const gravityFactor = gravityFactors[planet];
+            if (gravityFactor) {
+                document.getElementById('weightForm').style.display = 'none';
+                const calculatedWeight = earthWeight * gravityFactor;
+                document.getElementById('result').textContent = `Ton poids sur ${planet} serait de ${calculatedWeight.toFixed(2)} kg.`;
+            } else {
+                document.getElementById('result').textContent = "Please select a valid planet.";
+            }
+        });
